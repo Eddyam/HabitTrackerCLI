757aafe23ffec2d82425c90223e510685dcee3d7
EddYam <edder_yamir@hotmail.com>
2025-07-17 09:32:20 -0600

    feat: Añadir metodo para marcar completitud de habito y metodo para representar objeto

--- habit.py
+++ habit.py
@@ -7,3 +7,12 @@ class Habit:
         self.description = description
         self.completions = [] # Fecha y hora en la que se completo el habito.
 
+    def add_completion(self, timestap: datetime.datetime = None):
+        """Añade una marca de tiempo de completid al habito."""
+        if timestap is None:
+            timestap = datetime.datetime.now() # Usa hora actual, si no se prporciona hora.
+        self.completions.append(timestap) # Añade fecha y hora a la lista
+
+    def __str__(self):
+        """Representa el objeto como una cadena legible."""
+        return f"Hábito: {self.name} - Descripcion: {self.description} - Completado {len(self.completions)} veces."
