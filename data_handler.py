ca6119f8e150ded442e45689df0f75ddfa2a322c
EddYam <edder_yamir@hotmail.com>
2025-07-18 23:38:19 -0600

    feat: Añadir archivo data_handler.py encargado de guardar información y cargarla desde el archivo habits.json. Añadir función para cargar hábitos desde el archivo JSON.

---
+++ data_handler.py
@@ -0,0 +1,21 @@
+# data_handler.py
+import json
+from pathlib import Path
+from habit import Habit
+
+FILE_PATH = Path("habits.json") # Ruta del archivo JSON. 
+
+def load() -> list[Habit]:
+    """
+    Carga los hábitos desde el archivo JSON y los convierte en una lista de objetos.
+    """
+    if not FILE_PATH.exists():
+        return []
+    try:
+        with open(FILE_PATH, 'r', encoding='utf-8') as f:
+            data = json.load(f) # Carga el contenido del JSON
+            # Convierte cada diccionario a objetos.
+            return [Habit.from_dict(item) for item in data]
+    except json.JSONDecodeError:
+        print("Error: El archivo habits.json está corrupto o vacio. Se iniciará con hábitos vacios.")
+        return []
