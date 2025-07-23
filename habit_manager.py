c80aba0ca501d45c326b5047daccf0df06ac62ca
EddYam <edder_yamir@hotmail.com>
2025-07-22 19:10:31 -0600

    feat: Añadir métodos complete_habit y list_habits.

--- habit_manager.py
+++ habit_manager.py
@@ -29,6 +29,26 @@ class HabitManager:
         self.save_all_habits()
         print(f"Hábito {name} añadido.")
         return True
+    
+    def complete_habit(self, habit_name: str) -> bool: 
+        """Marca un hábito existente como completado con la fecha y hora actuales."""
+        for habit in self.habits: 
+            if habit.name.lower() == habit_name.lower():
+                habit.add_completion()
+                self.save_all_habits()
+                print(f"Hábito {habit_name} marcado como completado en {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.")
+                return True
+        print(f"Error: Hábito {habit_name} no encontrado.")
+
+    def list_habits(self):
+        """Imprime la lista de todos los hábitos y cuantas veces se han completado."""
+        if not self.habits:
+            print("No hay hábitos registrados todavía.")
+            return
+        print("\n----Tus hábitos ----")
+        for i, habit in enumerate(self.habits):
+            print(f"{i + 1}. {habit.name} ({len(habit.completions)} veces completado.)")
+        print("-------------------")
 
     def save_all_habits(self):
         """Metodo interno para guardar los hábitos actuales en el archivo JSON."""
