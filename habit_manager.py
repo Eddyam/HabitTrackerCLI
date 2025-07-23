7c78e03d6bf398b85a927c73f9f6af4db5e7ffe9
EddYam <edder_yamir@hotmail.com>
2025-07-22 18:51:20 -0600

    feat: Crear clase HabitManager y añadir metodos add_habit, save_all_habits.

--- habit_manager.py
+++ habit_manager.py
@@ -10,3 +10,26 @@ class Colors:
     YELLOW = '\033[93m'
     BLUE = '\033[94m'
     ENDC = '\033[0m'
+
+class HabitManager: 
+    def __init__(self):
+        """
+        Constructor del gestor. 
+        Carga todos los hábitos existentes desde el archivo JSON.
+        """
+        self.habits = load()
+
+    def add_habit(self, name: str, description: str = "") -> bool:
+        """Añade un nuevo hábito a la lista si no existe."""
+        if any(h.name.lower() == name.lower() for h in self.habits):
+            print(f"Error: El hábtio {name} ya existe.")
+            return False
+        new_habit = Habit(name, description) # Crea nuevo objeto Habit
+        self.habits.append(new_habit)
+        self.save_all_habits()
+        print(f"Hábito {name} añadido.")
+        return True
+
+    def save_all_habits(self):
+        """Metodo interno para guardar los hábitos actuales en el archivo JSON."""
+        save_habits(self.habits)
