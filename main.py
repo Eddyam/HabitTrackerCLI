42570d9c5f2f12a40a28c2cdb432d61f61ce41bf
EddYam <edder_yamir@hotmail.com>
2025-07-24 19:55:27 -0600

    feat: Añadir clase para definir colores para la terminal

---
+++ main.py
@@ -0,0 +1,7 @@
+# main. py
+from habit_manager import HabitManager
+from pathlib import Path
+
+class Colors: # Define colores para la terminal
+    GREEN = "\033[92m"
+    ENDC = "\033[0m" # Resetea el color
