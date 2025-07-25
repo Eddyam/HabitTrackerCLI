585052f82c8ac5539a649ef50ef6dbdc17a195a4
EddYam <edder_yamir@hotmail.com>
2025-07-24 19:59:44 -0600

    feat: Añadir función que muestra el menu de opciones

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ from pathlib import Path
 class Colors: # Define colores para la terminal
     GREEN = "\033[92m"
     ENDC = "\033[0m" # Resetea el color
+
+def display_menu():
+    """Muestra el menú de opciones al usuario."""
+    print(Colors.GREEN + "\n--- GESTOR DE HÁBITOS ---" + Colors.ENDC)
+    print("1. Añadir Hábito")
+    print("2. Marcar Hábito como Completado")
+    print("3. Listar Hábitos")
+    print("4. Ver Detalle del Hábito")
+    print("5. Eliminar Hábito")
+    print("6. Resumen General")
+    print("7. Salir")
+    print(Colors.GREEN + "----------------------------" + Colors.ENDC)
