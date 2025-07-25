0f93de03e800d69403b383e79cb26bd4cb151e46
EddYam <edder_yamir@hotmail.com>
2025-07-24 20:10:33 -0600

    feat: Añadir funcion principal que ejecuta la aplicación

--- main.py
+++ main.py
@@ -17,3 +17,37 @@ def display_menu():
     print("6. Resumen General")
     print("7. Salir")
     print(Colors.GREEN + "----------------------------" + Colors.ENDC)
+
+def main():
+    """Función principal que ejecuta la aplicación."""
+    manager = HabitManager() # Crea instancia del gestor de hábitos.
+
+    while True: 
+        display_menu()
+        choice = input("Elige un opción: ")
+
+        if choice == 1: 
+            name = input("Nombre del hábito: ")
+            description = input("Descripción (opcional)")
+            manager.add_habit(name, description) # Llama al manager para añadir un hábito.
+        elif choice == 2: 
+            name = input("Nombre del hábito a marcar como completad: ")
+            manager.complete_habit(name)
+        elif choice == 3: 
+            manager.list_habits()
+        elif choice == 4: 
+            name = input("Nombre del hábito para ver detalles: ")
+            manager.show_habit_details(name)
+        elif choice == 5: 
+            name = input("Nombre del hábito a eliminar: ")
+            manager.delete_habit(name)
+        elif choice == 6: 
+            manager.get_summary()
+        elif choice == 7:
+            print("!Hasta luego!")
+            break
+        else: 
+            print("Opción no válida. Inténtalo de nuevo.")
+
+if __name__ == "__main__":
+    main()
